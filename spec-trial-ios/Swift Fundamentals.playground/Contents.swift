7eda4d2a3258ded731c37ab2d1ffdd43af5bbd13
Brian Williams <heffergod@gmail.com>
2022-09-15 16:04:46 -0700

    midway through spec-trial-iOS

---
+++ spec-trial-ios/Swift Fundamentals.playground/Contents.swift
@@ -0,0 +1,20 @@
+
+
+let first_name = "Bobble"
+let last_name = "McFuzzlecrock"
+let birth_year = 1928
+let nationality = "Fromboozlian"
+let blood_status = "liquidy"
+let gender = "mixed"
+let height = 78
+let weight = 234
+
+
+let hpm_apear = false
+let fb_apear = false
+let da_member = true
+let de_member = false
+
+let importance = "indeterminable"
+let performance_rating = 0
+let why_fav = "Most of the characters are fairly 1-dimentional, and Bobble McFuzzlecrock lives entirely in 3D"
